be43105130afc144de54f4fe5c633e1b790cf455
eedsan <sazon.eugene@gmail.com>
2022-12-23 13:44:10 +0200

    create local database connection

---
+++ db_helper.py
@@ -0,0 +1,29 @@
+import os
+
+from mysql import connector
+from dotenv import load_dotenv
+
+
+def db_connection_open():
+    load_dotenv()
+    mydb = connector.connect(
+        host=os.environ["db_host"],
+        user=os.environ["db_user"],
+        password=os.environ["db_password"],
+        database=os.environ["db_database"])
+    return mydb
+
+
+def check_if_table_is_empty(cursor, table):
+    cursor.execute(f"SELECT (CASE WHEN NOT EXISTS(SELECT NULL FROM {table}) THEN 1 ELSE 0 END) AS isEmpty")
+    return cursor.fetchall()
+
+
+def is_user_email_in_db(my_cursor, user_email_):
+    my_cursor.execute("SELECT COUNT(*) FROM users WHERE user_email LIKE '{u_email}';".format(u_email=user_email_))
+    return my_cursor.fetchall()[0][0]
+
+
+def is_tag_in_db(my_cursor, tag_name_):
+    my_cursor.execute("SELECT COUNT(*) FROM tags WHERE tags.tag_name LIKE '{tag_n}';".format(tag_n=tag_name_))
+    return my_cursor.fetchall()[0][0]
