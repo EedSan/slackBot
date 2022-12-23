015362ce04349f8e3af073655ec07637fa5d7290
eedsan <sazon.eugene@gmail.com>
2022-12-23 18:46:04 +0200

    is user admin check debug

--- slack_helper.py
+++ slack_helper.py
@@ -1,5 +1,5 @@
 def is_user_admin(client, user_id):
-    user_info = client.users_info(user=user_id)
+    user_info = client.users_info(user=user_id)['user']
     return user_info['is_admin'] or user_info['is_owner']
 
 
