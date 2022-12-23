7e28e83839e0962fa7c1bc915e057e4e69733ab6
eedsan <sazon.eugene@gmail.com>
2022-12-23 15:38:51 +0200

    added help on channel invitation

--- message_handlers/display_help.py
+++ message_handlers/display_help.py
@@ -1,19 +1,38 @@
 import re
 
-from helper import is_private_message, is_user_admin
+from slack_helper import is_private_message, is_user_admin
 
 
 def help_invite_to_channel(user_id):
     """
-    :return: message to given user with help description
+    :return: message to given user with help description on invite users to channels
     """
-    text_ = "Some help on channel invitation."
-    return {"channel": user_id, "text": text_, }
+    general_invite_ = {"type": "section",
+                       "text": {"type": "mrkdwn",
+                                "text": ('To invite users channel, type: `invite to [-channel-name-] [-user-tag-]`\n'
+                                         '*channel-name*: parameter to specify the channel name.\n'
+                                         '*user-tag*: parameter to specify group the users belong to \n NOTE. names '
+                                         'of channels and user tags must be wrapped in separate squared brackets'), }, }
+    invite_one_ = {"type": "section",
+                   "text": {"type": "mrkdwn",
+                            "text": ('\tE. g.: `invite to [Data-Science] [KM-91]`\n'
+                                     '\t\tIn this particular case, users from the _KM-91_ group will be added '
+                                     ' to the channel called _Data-Science_'), }, }
+    invite_several_ = {"type": "section",
+                       "text": {"type": "mrkdwn",
+                                "text": ('You can invite many users to many channels with one command, for this, '
+                                         'please list the channel names separated by commas.\n'
+                                         '\tE. g. `create channel Data-Science, Data-Analysis, Machine-Learning`\n'
+                                         '\t\tIn this particular case, channels named '
+                                         '_Data-Science_, _Data-Analysis_, _Machine-Learning_ '
+                                         'will be created and you will be added to them.'), }, }
+    divider_ = {"type": "divider"}
+    return {"channel": user_id, "blocks": [general_invite_, divider_, invite_one_, divider_, invite_several_], }
 
 
 def help_create_channel(user_id):
     """
-    :return: message to given user with help description
+    :return: message to given user with help description on create channels
     """
     general_create_ = {"type": "section",
                        "text": {"type": "mrkdwn",
@@ -40,7 +59,7 @@ def help_create_channel(user_id):
 
 def general_help(user_id):
     """
-    :return: message to given user with help description
+    :return: message to given user with general help description
     """
     help_text_ = ('Welcome to SlackBOT help utility! \n'
                   '`*Write some thank words one first time usage`. *'
@@ -49,7 +68,7 @@ def general_help(user_id):
     return {"channel": user_id, "text": help_text_, }
 
 
-def disp_helps(message, client):
+def display_helps(message, client):
     print(message)
     if not is_private_message(message) or not is_user_admin(client, message['user']):
         return
