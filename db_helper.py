383f6f7ddb456a9533f75a3422fb101cdb4a7d8d
Holden <qqqwsq@gmail.com>
2023-01-04 17:57:52 +0200

    db_helper.py documentation rev. 1

--- db_helper.py
+++ db_helper.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 
 
 def db_connection_open():
+    """
+    Opens a connection to the database using enviromental variables.
+
+    :return: Database connection instance.
+    """
     load_dotenv()
     mydb = connector.connect(
         host=os.environ["db_host"],
@@ -15,15 +20,36 @@ def db_connection_open():
 
 
 def check_if_table_is_empty(cursor, table):
+    """
+    Checks if the specified table is empty.
+
+    :param cursor: Database connection cursor.
+    :param table: A table, the emptiness of which must be checked.
+    :return: Check result.
+    """
     cursor.execute(f"SELECT (CASE WHEN NOT EXISTS(SELECT NULL FROM {table}) THEN 1 ELSE 0 END) AS isEmpty")
     return cursor.fetchall()
 
 
 def is_user_email_in_db(my_cursor, user_email_):
+    """
+    Checks if the specified user email is in a table.
+
+    :param my_cursor: Database connection cursor.
+    :param user_email_: The email address of the user whose entry you want to check.
+    :return: Table records with a given email address.
+    """
     my_cursor.execute("SELECT COUNT(*) FROM users WHERE user_email LIKE '{u_email}';".format(u_email=user_email_))
     return my_cursor.fetchall()[0][0]
 
 
 def is_tag_in_db(my_cursor, tag_name_):
+    """
+    Checks if the specified user tag is in a table.
+
+    :param my_cursor: Database connection cursor.
+    :param tag_name_: Tag name, which entry is to be checked.
+    :return: Table records with a given tag.
+    """
     my_cursor.execute("SELECT COUNT(*) FROM tags WHERE tags.tag_name LIKE '{tag_n}';".format(tag_n=tag_name_))
     return my_cursor.fetchall()[0][0]
